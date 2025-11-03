2297d2bc54ccc88dee998485f8080933fd0f56fa
machageDev <machagefranklyn@gmail.com>
2025-11-03 23:01:19 +0300

    added state class . rep whats happening

---
+++ playgraund_dart/lib/bloc/login_state.dart
@@ -0,0 +1,18 @@
+// base state class for login feature
+abstract class LoginState {}
+
+// initial state when nothing has happened yet
+class LoginInitial extends LoginState {}
+// state when login process is ongoing
+class LoginLoading extends LoginState {}
+// state when login is successful
+class LoginSuccess extends LoginState {}
+// state when login fails eg wrong password
+class LoginFailure extends LoginState{
+  late final String message;
+// constructor to show error message
+loginFailure(this.message) {
+  // TODO: implement loginFailure
+  throw UnimplementedError();
+}
+}
