063c9be0ea1a2280a3a230ee8eee460261e04749
machageDev <machagefranklyn@gmail.com>
2025-11-06 14:07:04 +0300

    buttons style

---
+++ playgraund_dart/lib/buttons/buttons.dart
@@ -0,0 +1,11 @@
+
+
+
+TextButton(
+  style: ButtonStyle(
+    foregraoundColor: MaterialStateProperty.all<Color>(Colors.blue),
+  ),
+  onPressed: () {},
+  child: const Text('Text Button'),
+  )
+)
