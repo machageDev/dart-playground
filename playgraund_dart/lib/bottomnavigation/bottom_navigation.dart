af3c019f323c5d755786c3ad3b25623276ac893a
machageDev <machagefranklyn@gmail.com>
2025-11-06 21:58:31 +0300

    bottom navigation implementation

--- playgraund_dart/lib/bottomnavigation/bottom_navigation.dart
+++ playgraund_dart/lib/bottomnavigation/bottom_navigation.dart
@@ -1,49 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:playgraund_dart/buttons/buttons.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bottom Navigation Demo',
-      theme: ThemeData(primarySwatch: Colors.brown),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: BottomNavExample(),
-    )
+    );
   }
 }
 
 class BottomNavExample extends StatefulWidget {
   @override
   _BottomNavExampleState createState() => _BottomNavExampleState();
-
 }
 
 class _BottomNavExampleState extends State<BottomNavExample> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 0; // to track selected tab
 
-  //list of screens
+  // List of screens
   final List<Widget> _pages = [
     HomeScreen(),
+    SearchScreen(),
     ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
-    setState(()
-    {
-      _selectedIndex = index;// changes te screen
+    setState(() {
+      _selectedIndex = index; // change screen
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-
-    )
+      appBar: AppBar(title: Text('Bottom Navigation Example')),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
-  
+}
 
+// Individual Pages
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('🏠 Home Screen', style: TextStyle(fontSize: 24)));
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('🔍 Search Screen', style: TextStyle(fontSize: 24)));
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('👤 Profile Screen', style: TextStyle(fontSize: 24)));
+  }
 }
