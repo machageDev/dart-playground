feeeee91222e39f6e0a73adf239bdc1a374fc19a
machageDev <machagefranklyn@gmail.com>
2025-11-09 23:28:36 +0300

    events for forgotpassword

--- playgraund_dart/lib/bloc/forgot_event.dart
+++ playgraund_dart/lib/bloc/forgot_event.dart
@@ -1,1 +1,18 @@
- 
+ // lib/bloc/forgot_password_event.dart
+
+
+abstract class ForgotPasswordEvent extends Equatable {
+  const ForgotPasswordEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ForgotPasswordSubmitted extends ForgotPasswordEvent {
+  final String email;
+
+  const ForgotPasswordSubmitted(this.email);
+
+  @override
+  List<Object?> get props => [email];
+}
