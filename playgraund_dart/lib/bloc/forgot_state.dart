0241b863e40b80a4c505af119ffd208045c272b4
machageDev <machagefranklyn@gmail.com>
2025-11-09 23:29:33 +0300

    states for forgot ppassword added

---
+++ playgraund_dart/lib/bloc/forgot_state.dart
@@ -0,0 +1,21 @@
+// lib/bloc/forgot_password_state.dart
+import 'package:equatable/equatable.dart';
+
+abstract class ForgotPasswordState extends Equatable {
+  const ForgotPasswordState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ForgotPasswordInitial extends ForgotPasswordState {}
+class ForgotPasswordLoading extends ForgotPasswordState {}
+class ForgotPasswordSuccess extends ForgotPasswordState {}
+class ForgotPasswordFailure extends ForgotPasswordState {
+  final String error;
+
+  const ForgotPasswordFailure(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
