f30e35d6ca5e35d7fc6e2b1dcfd3f02adf5a15ea
machageDev <machagefranklyn@gmail.com>
2025-11-13 22:03:35 +0300

    Added User class with parameterized and named constructors for creating regular and guest users

---
+++ playgraund_dart/lib/contructors/constructor.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold (
+        appBar: AppBar(title: Text('Constructor Example')),
+        body: Center(child: Text('Hello, Flutter!')),
+      ),
+    );
+  }
+}
+
+
+class Person {
+  String name;
+  int age;
+
+  //constructor 
+  person(this.name, this.age);
+
+}
+
+void main() {
+  var p = person('Frank',22);
+  print('Name: ${p.name}, Age: ${p.age}');
+}
